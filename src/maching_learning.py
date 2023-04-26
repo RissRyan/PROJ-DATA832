1b578fef6f94c0b3f190b182adf707e985c8e9b9
Valentin <valentin.richard39@sfr.fr>
2023-04-26 23:18:38 +0200

    code final propre

---
+++ src/maching_learning.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import seaborn as sbn
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, DBSCAN, kmeans_plusplus
+
+
+def pca_on_dataframe (dataframe):
+    """
+    Appliquer une ACP sur un dataframe
+        Paramètres :
+            dataframe (pandas.Dataframe) : le dataframe à traiter
+
+        Sorties :
+            pca_data (list) : le résultat brut de l'ACP
+            pca_data_country_index (pandas.Dataframe) : le résultat de l'ACP avec les labels
+    """
+
+    # STANDARDISATION
+    scaler = StandardScaler()
+    cols = [col for col in dataframe.columns if col != 'country']
+    standardized_dataframe = dataframe.copy()
+    # Standardiser les données des colonnes sélectionnées
+    standardized_dataframe[cols] = scaler.fit_transform(standardized_dataframe[cols])
+
+    # REDUCTION DIMENSION ACP
+    pca = PCA(n_components=2)
+    pca_data = pca.fit_transform(standardized_dataframe[cols])
+
+    columns, country_index = ['PC1','PC2'], dataframe.country
+    pca_data_country_index = pd.DataFrame(pca_data, columns = columns, index=country_index)
+
+    return pca_data, pca_data_country_index
+
+
+def kmeans_clustering (dataframe, cluster_number):
+    """
+    Appliquer la méthode kmeans sur un jeu de données
+        Paramètres :
+            dataframe (pandas.Dataframe) : le dataframe à traiter
+            cluster_number (int) : le nombre de clusters
+
+        Aucune sortie
+    """
+
+    pca_data_country_index = pca_on_dataframe(dataframe)[1]
+
+    # CLUSTERING KMEANS
+    kmeans = KMeans(n_clusters=cluster_number,
+                    random_state=0,
+                    n_init=cluster_number).fit(pca_data_country_index)
+
+
+    # AJOUT DES LABELS DU KMEANS
+    kmean_df = pca_data_country_index.copy()
+    kmean_df['cluster'] = kmeans.labels_
+
+    # VOIR REPARTITION DES CLUSTERS
+    sbn.swarmplot(data=kmean_df.cluster)
+    plt.xlabel('Pays')
+    plt.ylabel('Numéro de cluster')
+    plt.title('Répartition des pays selon les clusters\nKmeans')
+    plt.show()
+
+    # SCATTER PLOTS DES POINTS ET COULEURS AVEC LES CLUSTERS
+    sbn.scatterplot(data=kmean_df, x='PC1', y='PC2', hue='cluster', palette="tab10")
+    plt.title('Répartition des pays\nKmeans')
+    plt.show()
+
+    # AFFICHER LA LISTE DES PAYS DANS CHAQUE CLUSTER
+    for i in range (cluster_number):
+        current_cluster_filter = kmean_df["cluster"].isin([i])
+        current_cluster = kmean_df[current_cluster_filter].index
+        print(current_cluster)
+
+
+
+def kmeans_plus_plus_clustering (dataframe, cluster_number) :
+    """
+    Appliquer la méthode kmeans sur un jeu de donnée avec une initialisation optimisée
+        Paramètres :
+            dataframe (pandas.Dataframe) : le dataframe à traiter
+            cluster_number (int) : le nombre de clusters
+
+        Aucune sortie
+    """
+
+    pca_data, pca_data_country_index = pca_on_dataframe(dataframe)
+
+
+    # CALCUL DE L'INITIALISATION AVEC KMEANS++
+    centers_init = kmeans_plusplus(pca_data, n_clusters=cluster_number, random_state=0)[0]
+
+    # CLUSTERING KMEANS
+    kmeans = KMeans(n_clusters=cluster_number,
+                    init=centers_init,
+                    n_init=1).fit(pca_data_country_index)
+
+
+    # AJOUT DES LABELS DU KMEANS
+    kmean_df = pca_data_country_index.copy()
+    kmean_df['cluster'] = kmeans.labels_
+
+    # VOIR REPARTITION DES CLUSTERS
+    sbn.swarmplot(data=kmean_df.cluster)
+    plt.xlabel('Pays')
+    plt.ylabel('Numéro de cluster')
+    plt.title('Répartition des pays selon les clusters\nKmeans++')
+    plt.show()
+
+    # SCATTER PLOTS DES POINTS ET COULEURS AVEC LES CLUSTERS
+    sbn.scatterplot(data=kmean_df, x='PC1', y='PC2', hue='cluster', palette="tab10")
+    plt.title('Répartition des pays\nKmeans++')
+    plt.show()
+
+    # AFFICHER LA LISTE DES PAYS DANS CHAQUE CLUSTER
+    for i in range (cluster_number):
+        current_cluster_filter = kmean_df["cluster"].isin([i])
+        current_cluster = kmean_df[current_cluster_filter].index
+        print(current_cluster)
+
+
+def bdscan_clustering (dataframe, eps, min_samples):
+    """
+    Appliquer la méthode DBSCAN sur un jeu de données
+        Paramètres :
+            dataframe (pandas.Dataframe) : le dataframe à traiter
+            eps (float) : l'espacement necessaire entre les points
+            min_samples (int) : le nombre de voisins necessaire
+
+        Aucune sortie
+    """
+
+    pca_data_country_index = pca_on_dataframe(dataframe)[1]
+
+    bdscan = DBSCAN(eps=eps, min_samples=min_samples).fit(pca_data_country_index)
+
+    dbscan_df = pca_data_country_index.copy()
+    dbscan_df['cluster'] = bdscan.labels_
+
+    # SCATTER PLOTS DES POINTS ET COULEURS AVEC LES CLUSTERS
+    sbn.scatterplot(data=dbscan_df, x='PC1', y='PC2', hue='cluster', palette="tab10")
+    plt.title('Répartition des pays\nBDSCAN')
+    plt.show()
+    plt.show()
--- src/visualisation_donnees.py
+++ src/visualisation_donnees.py
@@ -1,9 +1,6 @@
 import seaborn as sbn
 import matplotlib.pyplot as plt
 
-# print(countryDF.head())
-# print(countryDF.info())
-# Toutes les features sont des réels sauf "country"
 
 def afficher_moyennes_colonnes (dataframe):
     """
@@ -11,9 +8,9 @@ def afficher_moyennes_colonnes (dataframe):
         Paramètres :
             dataframe (pandas.Dataframe) : le dataframe à visualiser
 
-        Sorties :
-            bool : True
+        Aucune sortie
     """
+
     # Calculer la moyenne de chaque feature
     means = dataframe.mean(numeric_only=True)
     print("Moyenne de chaque feature :")
@@ -25,9 +22,9 @@ def afficher_variances_colonnes (dataframe):
         Paramètres :
             dataframe (pandas.Dataframe) : le dataframe à visualiser
 
-        Sorties :
-            bool : True
+        Aucune sortie
     """
+
     # Calculer la variance de chaque feature
     variances = dataframe.var(numeric_only=True)
     print("Variance de chaque feature :")
@@ -36,13 +33,13 @@ def afficher_variances_colonnes (dataframe):
 
 def afficher_matrice_covariance (dataframe):
     """
-    Affiche dans la console la matricce de covariance du dataframe.
+    Affiche dans la console la matrice de covariance du dataframe.
         Paramètres :
             dataframe (pandas.Dataframe) : le dataframe à visualiser
 
-        Sorties :
-            bool : True
+        Aucune sortie
     """
+
     # Calculer la matrice de corrélation
     corr_matrix = dataframe.corr(numeric_only=True)
 
@@ -52,11 +49,17 @@ def afficher_matrice_covariance (dataframe):
 
 
 def distribution_variable(dataframe, column):
-    #sbn.catplot(x='country', y=column, kind='box', data=dataframe)
-    sbn.displot(dataframe, x=column)
-    # Afficher les graphiques
-    plt.show()
+    """
+    Affiche dans la distribution d'une variable d'un dataframe
+        Paramètres :
+            dataframe (pandas.Dataframe) : le dataframe qui comporte la donnée
+            column (str) : le nom de la colonne à visualiser
 
+        Aucune sortie
+    """
+
+    sbn.displot(dataframe, x=column)
+    plt.show()
 
 
 def top_countries_by_feature(dataframe, feature, number_element=10, ascending_param=False):
@@ -68,8 +71,7 @@ def top_countries_by_feature(dataframe, feature, number_element=10, ascending_pa
             number_element (int) : nombre des elements à afficher (default = 10)
             ascending_param (bool) : affichage par ordre croissant ou non (default = False)
 
-        Sorties :
-            bool : True
+        Aucune sortie
     """
 
     # Trier le DataFrame en fonction de la feature choisie
@@ -84,6 +86,4 @@ def top_countries_by_feature(dataframe, feature, number_element=10, ascending_pa
     plt.ylabel(feature)
     ordre = 'faibles' if ascending_param else 'élevées'
     plt.title(f"Top {number_element} pays avec les valeurs les plus {ordre} pour '{feature}'")
-
     plt.show()
-    return True
